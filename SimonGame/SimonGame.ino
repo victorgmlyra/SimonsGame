1f8e3ea15b96e04131e2bc3f729fd713a1196923
victorgmlyra <victorgmlyra@gmail.com>
2022-10-19 17:57:29 +0200

    Commenting wifi related code

--- SimonGame/SimonGame.ino
+++ SimonGame/SimonGame.ino
@@ -1,14 +1,14 @@
 #include <LiquidCrystal_I2C.h>
-#include <ESP8266WiFi.h>  
-#include "src/iotc/common/string_buffer.h"
-#include "src/iotc/iotc.h"
+// #include <ESP8266WiFi.h>  
+// #include "src/iotc/common/string_buffer.h"
+// #include "src/iotc/iotc.h"
 
 // Wifi and Azure Configs
-#define WIFI_SSID "Pixel_2704"
-#define WIFI_PASSWORD "123456789"
-const char* SCOPE_ID = "0ne007D56E7";
-const char* DEVICE_ID = "yed4c6sehx";
-const char* DEVICE_KEY = "+rGkkOXQWUX3rTPAt8zfJRBLq47giNTubU8ClUKDUvE=";
+// #define WIFI_SSID "Pixel_2704"
+// #define WIFI_PASSWORD "123456789"
+// const char* SCOPE_ID = "0ne007D56E7";
+// const char* DEVICE_ID = "yed4c6sehx";
+// const char* DEVICE_KEY = "+rGkkOXQWUX3rTPAt8zfJRBLq47giNTubU8ClUKDUvE=";
 
 // Push-buttons definitions
 #define RED_BUTTON D3 
@@ -36,9 +36,9 @@ void showSequence();
 void inputSequence();
 int listenInput(int LEDdelay);
 void lightLed(int ledNum, int LEDdelay);
-void on_event(IOTContext ctx, IOTCallbackInfo* callbackInfo);
-void sendTelemetry(char* var_msg, int value);
-#include "src/connection.h"
+// void on_event(IOTContext ctx, IOTCallbackInfo* callbackInfo);
+// void sendTelemetry(char* var_msg, int value);
+// #include "src/connection.h"
 
 // Variables
 uint8_t LEDs[] = {RED_LED, GREEN_LED, YELLOW_LED, BLUE_LED};
@@ -69,19 +69,19 @@ void setup() {
   lcd.setCursor(0, 1);
   lcd.print("to WIFI...");
 
-  // WIFI and Azure
-  connect_wifi(WIFI_SSID, WIFI_PASSWORD);
-  connect_client(SCOPE_ID, DEVICE_ID, DEVICE_KEY);
-  if (context != NULL) {
-    lastTick = 0;  // set timer in the past to enable first telemetry a.s.a.p
-  }
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  if (isConnected)
-    lcd.print("Connected!");
-  else
-    lcd.print("Not Connected :(");
-  delay(10000);
+  // // WIFI and Azure
+  // connect_wifi(WIFI_SSID, WIFI_PASSWORD);
+  // connect_client(SCOPE_ID, DEVICE_ID, DEVICE_KEY);
+  // if (context != NULL) {
+  //   lastTick = 0;  // set timer in the past to enable first telemetry a.s.a.p
+  // }
+  // lcd.clear();
+  // lcd.setCursor(0, 0);
+  // if (isConnected)
+  //   lcd.print("Connected!");
+  // else
+  //   lcd.print("Not Connected :(");
+  // delay(10000);
 
   // Setup Push-buttons
   pinMode(RED_BUTTON, INPUT_PULLUP);
@@ -235,7 +235,7 @@ void playAgain()
 {
   if (currLen > maxLen)
     maxLen = currLen;
-  sendTelemetry("Max_length", maxLen);
+  // sendTelemetry("Max_length", maxLen);
   int buttonPressed = listenInput(300);
   if (buttonPressed == 1){
     gameMode = 0;
@@ -260,7 +260,7 @@ void showSequence()
   currLen++;
   lcd.setCursor(13, 0);
   lcd.print(currLen);
-  sendTelemetry("Curr_length", currLen);
+  // sendTelemetry("Curr_length", currLen);
   for(int i = 0; i < currLen; i++){
     lightLed(sequence[i], 400);
     delay(100);
@@ -396,52 +396,52 @@ void lightLed(int ledNum, int LEDdelay)
     WIFI AND AZURE FUNCTIONS
 ********************************/
 
-void on_event(IOTContext ctx, IOTCallbackInfo* callbackInfo) {
-  // ConnectionStatus
-  if (strcmp(callbackInfo->eventName, "ConnectionStatus") == 0) {
-    LOG_VERBOSE("Is connected ? %s (%d)",
-                callbackInfo->statusCode == IOTC_CONNECTION_OK ? "YES" : "NO",
-                callbackInfo->statusCode);
-    isConnected = callbackInfo->statusCode == IOTC_CONNECTION_OK;
-    return;
-  }
+// void on_event(IOTContext ctx, IOTCallbackInfo* callbackInfo) {
+//   // ConnectionStatus
+//   if (strcmp(callbackInfo->eventName, "ConnectionStatus") == 0) {
+//     LOG_VERBOSE("Is connected ? %s (%d)",
+//                 callbackInfo->statusCode == IOTC_CONNECTION_OK ? "YES" : "NO",
+//                 callbackInfo->statusCode);
+//     isConnected = callbackInfo->statusCode == IOTC_CONNECTION_OK;
+//     return;
+//   }
 
-  // payload buffer doesn't have a null ending.
-  // add null ending in another buffer before print
-  AzureIOT::StringBuffer buffer;
-  if (callbackInfo->payloadLength > 0) {
-    buffer.initialize(callbackInfo->payload, callbackInfo->payloadLength);
-  }
+//   // payload buffer doesn't have a null ending.
+//   // add null ending in another buffer before print
+//   AzureIOT::StringBuffer buffer;
+//   if (callbackInfo->payloadLength > 0) {
+//     buffer.initialize(callbackInfo->payload, callbackInfo->payloadLength);
+//   }
 
-  LOG_VERBOSE("- [%s] event was received. Payload => %s\n",
-              callbackInfo->eventName, buffer.getLength() ? *buffer : "EMPTY");
+//   LOG_VERBOSE("- [%s] event was received. Payload => %s\n",
+//               callbackInfo->eventName, buffer.getLength() ? *buffer : "EMPTY");
 
-  if (strcmp(callbackInfo->eventName, "Command") == 0) {
-    LOG_VERBOSE("- Command name was => %s\r\n", callbackInfo->tag);
-  }
-}
+//   if (strcmp(callbackInfo->eventName, "Command") == 0) {
+//     LOG_VERBOSE("- Command name was => %s\r\n", callbackInfo->tag);
+//   }
+// }
 
-void sendTelemetry(char* var_msg, int value)
-{
-  if (isConnected) {
-    char msg[64] = {0};
-    int pos = 0, errorCode = 0;
+// void sendTelemetry(char* var_msg, int value)
+// {
+//   if (isConnected) {
+//     char msg[64] = {0};
+//     int pos = 0, errorCode = 0;
 
-    pos = snprintf(msg, sizeof(msg) - 1, "{\"%s\": %d}", var_msg, value);
-    // else
-    //   pos = snprintf(msg, sizeof(msg) - 1, "{\"Max_length\": %d}", value);
-    errorCode = iotc_send_telemetry(context, msg, pos);
+//     pos = snprintf(msg, sizeof(msg) - 1, "{\"%s\": %d}", var_msg, value);
+//     // else
+//     //   pos = snprintf(msg, sizeof(msg) - 1, "{\"Max_length\": %d}", value);
+//     errorCode = iotc_send_telemetry(context, msg, pos);
 
-    msg[pos] = 0;
+//     msg[pos] = 0;
 
-    if (errorCode != 0) {
-      LOG_ERROR("Sending message has failed with error code %d", errorCode);
-    }
+//     if (errorCode != 0) {
+//       LOG_ERROR("Sending message has failed with error code %d", errorCode);
+//     }
 
-    iotc_do_work(context);  // do background work for iotc
-  } else {
-    iotc_free_context(context);
-    context = NULL;
-    connect_client(SCOPE_ID, DEVICE_ID, DEVICE_KEY);
-  }
-}
+//     iotc_do_work(context);  // do background work for iotc
+//   } else {
+//     iotc_free_context(context);
+//     context = NULL;
+//     connect_client(SCOPE_ID, DEVICE_ID, DEVICE_KEY);
+//   }
+// }
